aef00afe066cb1c42536887f95be3ca1fcac339d
robson.koji <rbsnkjmr@gmail.com>
2013-12-13 00:17:33 -0200

    README

--- templates/sql_file.sql
+++
@@ -1,4 +0,0 @@
-    [% FOREACH choice_id = sql.choices.keys %]
-        INSERT INTO [% sql.slug %]_choices_[% sql.field_slug %] (choice_desc) VALUES ('[% sql.choices.$choice_id.label %]');
-    [% END %]  
-
