a5362b25945dc9412fd3fb1b40503e24216c3f2c
fortinbras <robson.koji@gmail.com>
2013-12-17 10:54:28 -0200

    Liberando templates/settings.py para o Perl, e mudanças menores no config.template.pl e hosts.template.pl

--- making_software_perl/hosts.template.pl
+++ making_software_perl/hosts.template.pl
@@ -1,7 +1,9 @@
 ##############################################
 #                                            # 
 #  You have to rename this file to hosts.pl  #
-#                                            #  
+#  Just rename and don´t change it if        #
+#  you don´t need more than one environment. #
+#                                            # 
 ##############################################
 
 
@@ -14,5 +16,5 @@
 # For other environments you may use the key you want.
 
 # This is an example. Change the values for your environments.
-prod => ['my_prod_host_name', 'config_prod.pl'],
-devel => ['my_devel_host_name', 'config_devel.pl'],
+#prod => ['my_prod_host_name', 'config_prod.pl'],
+#devel => ['my_devel_host_name', 'config_devel.pl'],
